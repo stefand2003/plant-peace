3985820b4dd037303a2f638eae71f8e64234ce5d
stefand2003 <119484929+stefand2003@users.noreply.github.com>
2023-06-09 10:03:47 +0100

    Added color pallete and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.scss';
+import Color from './assets/color/color';
 
 function App() {
-  return;
+  return (
+    <div>
+      <Color />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/assets/Color/Color.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Color.scss';
+
+export default function Color() {
+  return (
+    <div className='colors'>
+      <ul>
+        <li>Olive Green</li>
+        <li>Dark Green</li>
+      </ul>
+    </div>
+  );
+}
